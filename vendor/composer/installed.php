cd6baff20a678594a392c14ad6c2c9e80ee3bb73
denavia_gt <anggitadenav@gmail.com>
2022-04-09 16:41:30 +0700

    test

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '374ee71a53d7f990f1b9695120634fba11adb1e6',
+        'reference' => 'a32473619216a08844aebeb6cd29ad5e8c677e1d',
         'name' => 'codeigniter/framework',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '374ee71a53d7f990f1b9695120634fba11adb1e6',
+            'reference' => 'a32473619216a08844aebeb6cd29ad5e8c677e1d',
             'dev_requirement' => false,
         ),
         'doctrine/instantiator' => array(
