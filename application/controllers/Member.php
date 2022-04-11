e54622413fdef15560973140c72250aefb5c009d
denavia_gt <anggitadenav@gmail.com>
2022-04-11 13:08:41 +0700

    Member edit

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -47,15 +47,32 @@ class Member extends CI_Controller {
     {
         $dataid = array('idKonsumen' => $id);
         $data['member'] = $this->M_crud->get_by_id('tbl_member', $dataid)->row_object();
+        $data['kota'] =  $this->M_crud->get_all_data('tbl_kota')-> result();
         $this->template->load('layout_admin', 'admin/member/form_edit', $data);
     }
 
     public function update()
     {
+        // var_dump($this->input->post('telp')); die();
         $id = $this->input->post('id');
 
-        $category_name = $this->input->post('category_name');
-        $data = array('namaKonsumen' => $category_name);
+        $username = $this->input->post('username');
+        $email = $this->input->post('email');
+        $namaKonsumen = $this->input->post('namaKonsumen');
+        $idKota = $this->input->post('idKota');
+        $alamat = $this->input->post('alamat');
+        $telp = $this->input->post('telp');
+        $status = $this->input->post('status');
+ 
+        $data = array(
+                     'username' => $username, 
+                     'email' => $email,
+                     'namaKonsumen' => $namaKonsumen,
+                     'idKota' => $idKota,
+                     'alamat' => $alamat,
+                     'tlpn' => $telp,
+                     'statusAktif' => $status,
+                     'password' => 'password');
         $this->M_crud->update('tbl_member', $data, 'idKonsumen', $id);
         redirect('member');
     }
